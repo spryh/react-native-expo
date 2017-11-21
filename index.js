4724aa7ef8a13be41d8487757a89f9415f4350f8
Spryh <skyphr@gmail.com>
2017-11-21 14:29:29 -0800

    Documentjs

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-AppRegistry.registerComponent('albums', () => App);
+// Import library to help create a component
+import React from 'react'
+import ReactNative from 'react-native'
+
+// Create a component
+
+/**
+ * Adds two numbers
+ * @param {number} p1 
+ * @param {number} p2 
+ */
+function addsTwo(p1, p2) {
+    return p1 * p2;              // The function returns the product of p1 and p2
+}
+
+addsTwo('d', 1)
+// Render it to the device
