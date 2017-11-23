1f926e7981639d44b9180e8da676317a93a29fe9
Paul Scarpa <spryh@Bootstrap.home>
2017-11-23 14:47:02 -0800

    Thru 5.27 - Shadow Styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,32 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 
 const Header = () => {
-  const {textStyle} = styles
-  return <Text style = {textStyle}>Albums App!</Text>
+  const { textStyle, viewStyle } = styles
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>The Albums App</Text>
+    </View>
+  )
 }
 
 const styles = {
   textStyle: {
-    fontSize: 20 // RN uses camelCase on the styling
+    fontSize: 30 // RN uses camelCase on the styling
+  }, // Separate with comma
+
+  viewStyle: {
+    backgroundColor: '#f8f8f8', // justifyContent alignItems to center in the container
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2, // For some reason this is required?
+    position: 'relative'
   }
 }
 export default Header
-
