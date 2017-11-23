4fba4accba97b61261c2017b8a60b09d8aacc0ab
Paul Scarpa <spryh@Bootstrap.home>
2017-11-23 14:15:37 -0800

    Thru 5.24 - Styling

--- App.js
+++ App.js
@@ -4,8 +4,6 @@ import Header from './src/components/header'
 
 export default class App extends React.Component {
   render () {
-    return (
-        <Header />
-    )
+    return <Header />
   }
 }
--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,14 @@ import React from 'react'
 import { Text } from 'react-native'
 
 const Header = () => {
-  return <Text>Albums App!</Text>
+  const {textStyle} = styles
+  return <Text style = {textStyle}>Albums App!</Text>
 }
 
+const styles = {
+  textStyle: {
+    fontSize: 20 // RN uses camelCase on the styling
+  }
+}
 export default Header
+
