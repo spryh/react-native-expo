4d7702f8bcbe99f0cd7bdc3fa12ef920e73ad691
Paul Scarpa <spryh@Bootstrap.local>
2017-11-24 14:21:49 -0800

    Thru 5.32 Console Loggining & Debug breakpoint

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,8 +4,11 @@ import { Text, View } from 'react-native'
 // Class Component for handling dynamic data 5.31
 
 class AlbumList extends Component {
-  // const { textStyle, viewStyle } = styles
-
+  componentWillMount () {
+    // Shake expo phone and Debug Remotely in local browser
+    console.log('componentWillMount in AlbumList')
+    debugger
+  }
   render () {
     return (
       <View>
@@ -13,25 +16,6 @@ class AlbumList extends Component {
       </View>
     )
   }
-
-  /* const styles = {
-  textStyle: {
-    fontSize: 30
-  },
-
-  viewStyle: {
-    backgroundColor: '#f8f8f8',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 60,
-    paddingTop: 15,
-    shadowColor: 'black',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    elevation: 2,
-    position: 'relative'
-  }
-} */
 }
 
 export default AlbumList
