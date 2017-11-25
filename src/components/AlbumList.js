29d57514171979b8b349f93ba785bb8828d2c422
Paul Scarpa <spryh@Bootstrap.home>
2017-11-25 01:42:04 -0800

    Thru 5.35 Component State

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,25 +4,31 @@ import { Text, View } from 'react-native'
 // Class Component for handling dynamic data 5.31
 
 class AlbumList extends Component {
-  componentWillMount () {
+  state = { albums: [] }
+
+  componentWillMount() {
     console.log('AlbumList Component Mounted')
-    // Shake expo phone and Debug Remotely in local browser
-    // Use debugger and Console Sources to hit a breakpoint
-    // console.log('https://rallycoding.herokuapp.com/api/music_albums')
     fetch('https://rallycoding.herokuapp.com/api/music_albums')
       .then(fetched => fetched.json())
-      .then(fetchedJson => {
-        console.log(fetchedJson)
+      // This is the array of Json objects
+      .then(fetchedJsonArray => {
+        console.log(fetchedJsonArray)
+        console.log(fetchedJsonArray[0].artist)
+        //console.log(fetchedJson)
+        
+        // Component level state - set the state of Albumlist.state.albums to
+        this.setState({ albums: fetchedJson })
       })
       .catch(error => {
         console.error(error)
       })
   }
 
-  render () {
+  render() {
+    console.log(this.state)
     return (
       <View>
-        <Text>Album List 2</Text>
+        <Text>Album List</Text>
       </View>
     )
   }
