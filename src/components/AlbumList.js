547ebec3d871f12cb5bb1d047499f8d2c6659d69
Paul Scarpa <spryh@Bootstrap.home>
2017-11-25 00:54:24 -0800

    to 5.34 - Network Requests with fetch

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -5,14 +5,24 @@ import { Text, View } from 'react-native'
 
 class AlbumList extends Component {
   componentWillMount () {
+    console.log('AlbumList Component Mounted')
     // Shake expo phone and Debug Remotely in local browser
-    console.log('componentWillMount in AlbumList')
-    debugger
+    // Use debugger and Console Sources to hit a breakpoint
+    // console.log('https://rallycoding.herokuapp.com/api/music_albums')
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(fetched => fetched.json())
+      .then(fetchedJson => {
+        console.log(fetchedJson)
+      })
+      .catch(error => {
+        console.error(error)
+      })
   }
+
   render () {
     return (
       <View>
-        <Text>AlbumList</Text>
+        <Text>Album List 2</Text>
       </View>
     )
   }
